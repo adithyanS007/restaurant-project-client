6ab71806ce6a77d5b93e7c297152d29bdc758559
adithyanS007 <adhiadithyan21@gmail.com>
2025-01-28 08:31:00 +0530

    url uptd

--- src/Components/MenuItems.jsx
+++ src/Components/MenuItems.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
 
 const MenuItems = () => {
     const [selectedCategory, setSelectedCategory] = useState(null); // Track the selected category
